9deafcf2549e830a728ec85d7b6edcf1527f98cb
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 07:47:41 -0500

    chore: Update CriptoSearchForm component

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -2,6 +2,22 @@
 
 export const CriptoSearchForm = () => {
   return (
-    <div>CriptoSearchForm</div>
+    <form className="form">
+      <div className="field">
+        <label htmlFor="currency">Moneda:</label>
+        <select name="currency" id="criptoCurrency">
+          <option value="">--Seleccione --</option>
+        </select>
+      </div>
+
+      <div className="field">
+        <label htmlFor="criptoCurrency">Moneda:</label>
+        <select name="criptoCurrency" id="criptoCurrency">
+          <option value="">--Seleccione --</option>
+        </select>
+      </div>
+
+      <input type="submit" value='Cotizar'/>
+    </form>
   )
 }
