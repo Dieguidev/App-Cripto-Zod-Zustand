ee4f3ef9d27635f0854fe20ec8255d7e8bc0440d
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 09:28:19 -0500

    feat: Update CriptoSearchForm component with dynamic options
    The commit updates the CriptoSearchForm component to dynamically populate the options for both the "Moneda" and "criptoCurrency" select elements. It uses the `currencies` and `cryptoCurrencies` data from the store to generate the options. This enhancement improves the user experience by providing up-to-date and relevant currency and cryptocurrency options in the form.

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,18 +1,20 @@
-import { currencies } from "../data"
-
+import { currencies } from "../data";
+import { useCryptoStore } from "../store";
 
 export const CriptoSearchForm = () => {
+  const { cryptoCurrencies } = useCryptoStore();
+
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Moneda:</label>
         <select name="currency" id="criptoCurrency">
           <option value="">--Seleccione --</option>
-          {
-            currencies.map(currency => (
-              <option value={currency.code} key={currency.code}>{currency.name}</option>
-            ))
-          }
+          {currencies.map((currency) => (
+            <option value={currency.code} key={currency.code}>
+              {currency.name}
+            </option>
+          ))}
         </select>
       </div>
 
@@ -20,10 +22,15 @@ export const CriptoSearchForm = () => {
         <label htmlFor="criptoCurrency">Moneda:</label>
         <select name="criptoCurrency" id="criptoCurrency">
           <option value="">--Seleccione --</option>
+          {
+            cryptoCurrencies.map(crypto => (
+              <option value={crypto.CoinInfo.Name} key={crypto.CoinInfo.FullName}>{crypto.CoinInfo.FullName}</option>
+            ))
+          }
         </select>
       </div>
 
-      <input type="submit" value='Cotizar'/>
+      <input type="submit" value="Cotizar" />
     </form>
-  )
-}
+  );
+};
