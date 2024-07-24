4f85501b150063676c98757266bb8ab376a7d2db
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 09:36:22 -0500

    feat: Update add state local from form

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,14 +1,28 @@
+import { ChangeEvent, useState } from "react";
 import { currencies } from "../data";
 import { useCryptoStore } from "../store";
+import { Pair } from "../types";
 
 export const CriptoSearchForm = () => {
   const { cryptoCurrencies } = useCryptoStore();
+
+  const [pair, setPair] = useState<Pair>({
+    currency: "",
+    criptoCurrency: "",
+  });
+
+  const hadleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setPair({
+      ...pair,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Moneda:</label>
-        <select name="currency" id="criptoCurrency">
+        <select name="currency" id="criptoCurrency" onChange={hadleChange}>
           <option value="">--Seleccione --</option>
           {currencies.map((currency) => (
             <option value={currency.code} key={currency.code}>
@@ -20,13 +34,17 @@ export const CriptoSearchForm = () => {
 
       <div className="field">
         <label htmlFor="criptoCurrency">Moneda:</label>
-        <select name="criptoCurrency" id="criptoCurrency">
+        <select
+          name="criptoCurrency"
+          id="criptoCurrency"
+          onChange={hadleChange}
+        >
           <option value="">--Seleccione --</option>
-          {
-            cryptoCurrencies.map(crypto => (
-              <option value={crypto.CoinInfo.Name} key={crypto.CoinInfo.FullName}>{crypto.CoinInfo.FullName}</option>
-            ))
-          }
+          {cryptoCurrencies.map((crypto) => (
+            <option value={crypto.CoinInfo.Name} key={crypto.CoinInfo.FullName}>
+              {crypto.CoinInfo.FullName}
+            </option>
+          ))}
         </select>
       </div>
 
