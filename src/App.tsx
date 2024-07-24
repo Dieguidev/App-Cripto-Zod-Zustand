2beecfde47e2cf022c7a3d54d7d85437be7bf9d7
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 07:39:11 -0500

    feat: Add CriptoSearchForm component
    The commit adds a new component called CriptoSearchForm to the project. This component will be responsible for rendering the search form for cryptocurrencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-
+import { CriptoSearchForm } from "./components/CriptoSearchForm";
 
 function App() {
-
-
   return (
     <>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+      </div>
 
+      <div className="content">
+        <CriptoSearchForm/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CriptoSearchForm.tsx
@@ -0,0 +1,7 @@
+
+
+export const CriptoSearchForm = () => {
+  return (
+    <div>CriptoSearchForm</div>
+  )
+}
