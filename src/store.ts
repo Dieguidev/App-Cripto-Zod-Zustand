266ca40237ea0d8e4b3805765bde9888a250f6be
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 10:19:47 -0500

    feat: Add CryptoPrice type and update store to include crypto price data

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,37 @@
 import { create } from "zustand";
 import { devtools } from 'zustand/middleware'
 
-import { CryptoCurrency, Pair } from "./types";
+import { CryptoCurrency, CryptoPrice, Pair } from "./types";
 import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService";
 
 type CryptoStore = {
   cryptoCurrencies: CryptoCurrency[];
+  result: CryptoPrice;
   fetchCryptos: () => Promise<void>;
   fetchData: (pair: Pair) => Promise<void>;
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
   cryptoCurrencies: [],
+  result: {
+    IMAGEURL: '',
+    PRICE: '',
+    HIGHDAY: '',
+    LOWDAY: '',
+    CHANGEPCT24HOUR: '',
+    LASTUPDATE: '',
+  },
   fetchCryptos: async () => {
     const cryptoCurrencies = await getCryptos()
     set(() => ({
       cryptoCurrencies
     }))
   },
-  fetchData: async (pair)=>{
+  fetchData: async (pair) => {
     const result = await fetchCurrentCryptoPrice(pair)
-    console.log(result);
+    set(() => ({
+      result
+    }))
 
   }
 
